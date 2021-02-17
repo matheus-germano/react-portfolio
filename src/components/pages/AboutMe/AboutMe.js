b71927faabf7906416ee88d94b927feeb0f8727c
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-02-17 20:46:40 -0300

    Update AboutMe.js

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
           <div className="about-text">
             <h1>Sobre Mim</h1>
             <p>Tenho 17 anos, moro em Campinas-SP, técnico em informática formado pela Escola Salesiana São José em 2020, sou um desenvolvedor Front-End e UI/UX Designer.</p>
-            <a href="https://essj-my.sharepoint.com/:w:/g/personal/20190188_edu_essj_com_br/EX9sEw4K-zhLhbWobBmUGvIB0ww_VywZO1UhImByFsrTyg?e=b7o34e" className="btn btn-primary" target="_blank" rel="noopener noreferrer">Meu currículo</a>
+            <a href="https://drive.google.com/file/d/1p4fM53PFdngR5-H9NUY3N6C_L7k_qxLF/view?usp=sharing" className="btn btn-primary" target="_blank" rel="noopener noreferrer">Meu currículo</a>
           </div>
           <img src={AboutMeSVG} alt=""/>
         </div>
