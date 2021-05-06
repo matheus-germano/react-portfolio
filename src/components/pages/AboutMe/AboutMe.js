be8775cc8a99de009f75f0089ede5415c88f0eb3
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-05-06 10:40:35 -0300

    Age fix

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
         <div className="about-content">
           <div className="about-text">
             <h1>Sobre Mim</h1>
-            <p>Tenho 17 anos, moro em Campinas-SP, técnico em informática formado pela Escola Salesiana São José em 2020, sou um desenvolvedor Front-End e UI/UX Designer.</p>
+            <p>Tenho 18 anos, moro em Campinas-SP, técnico em informática formado pela Escola Salesiana São José em 2020, sou um desenvolvedor Front-End e UI/UX Designer.</p>
             <a href="https://drive.google.com/file/d/1p4fM53PFdngR5-H9NUY3N6C_L7k_qxLF/view?usp=sharing" className="btn btn-primary" target="_blank" rel="noopener noreferrer">Meu currículo</a>
           </div>
           <img src={AboutMeSVG} alt=""/>
